2a87f4414dbfbe937937759fffba5939d7048e86
Balcihatice <aciceaybike@gmail.com>
2023-02-16 14:11:58 +0300

    Api-Test

---
+++ src/test/java/com/apitest/day04/GetRequest01.java
@@ -0,0 +1,14 @@
+package com.apitest.day04;
+
+import io.restassured.internal.mapping.JsonbMapper;
+
+public class GetRequest01 {
+    //https://restful-booker.herokuapp.com/booking/3 adresine bir request gonderildiginde
+    // donecek cevap(response) icin
+    //>>HTTP satatus kodunun 200
+    //>>Content type in JsonbMapper
+    //>>ve status Line`in HTTP/1.1 200 OK oldugunu test edin
+
+
+
+}
