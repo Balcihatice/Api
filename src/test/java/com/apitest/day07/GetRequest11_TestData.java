ee4c5c082eb8368806085cbca377ed67992ecbfb
Balcihatice <aciceaybike@gmail.com>
2023-02-18 00:14:12 +0300

    Api-Test

---
+++ src/test/java/com/apitest/day07/GetRequest11_TestData.java
@@ -0,0 +1,66 @@
+package com.apitest.day07;
+
+import com.apitest.testBase.JsonPlaceHolderTestBase;
+import com.apitest.testData.JsonPlaceHolderTestData;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class GetRequest11_TestData extends JsonPlaceHolderTestBase {
+
+    @Test
+    public void test() {
+        spec01.pathParams("parametre1", "todos",
+                "parametre2", 2);
+
+        JsonPlaceHolderTestData expectedObje = new JsonPlaceHolderTestData();
+        HashMap<String, Object> expectedData = (HashMap<String, Object>) expectedObje.setUpTestData();
+        System.out.println(expectedData);
+
+
+        System.out.println("====================================");
+        Response response = given().
+                accept("application/json").
+                spec(spec01).
+                when().
+                get("/{parametre1}/{parametre2}");
+        response.prettyPrint();
+
+
+        //1.Yontem Matchers class ile assertion yaptik
+            response.then().
+                    assertThat().
+                    statusCode((Integer) expectedData.get("statusCode")).
+                    headers("via", equalTo(expectedData.get("via")),
+                            "Server", equalTo(expectedData.get("Server"))).
+                    body("userId", equalTo(expectedData.get("userId")),
+                            "title", equalTo(expectedData.get("title")),
+                            "completed", equalTo(expectedData.get("completed")));
+
+
+        //2.Yontem
+        JsonPath jsonPath = response.jsonPath();
+        Assert.assertEquals(expectedData.get("statusCode"), response.statusCode());
+        Assert.assertEquals(expectedData.get("Server"), response.getHeader("Server"));
+        Assert.assertEquals(expectedData.get("via"), response.getHeader("via"));
+        Assert.assertEquals(expectedData.get("userId"), jsonPath.getInt("userId"));
+        Assert.assertEquals(expectedData.get("title"), jsonPath.getString("title"));
+        Assert.assertEquals(expectedData.get("completed"), jsonPath.getBoolean("completed"));
+
+
+        //3.yontem deserialization
+        // --object mapper
+        // --pojo class ile birlikte map kullanacagiz
+
+
+    }
+
+}
---
+++ src/test/java/com/apitest/testData/JsonPlaceHolderTestData.java
@@ -0,0 +1,20 @@
+package com.apitest.testData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonPlaceHolderTestData {
+
+
+    public Map<String, Object> setUpTestData() {
+
+        HashMap<String, Object> expectedData = new HashMap<String, Object>();
+        expectedData.put("statusCode", 200);
+        expectedData.put("via", "1.1 vegur");
+        expectedData.put("title", "quis ut nam facilis et officia qui");
+        expectedData.put("userId", 1);
+        expectedData.put("Server", "cloudflare");
+        expectedData.put("completed",false);
+        return expectedData;
+    }
+}
