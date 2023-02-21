f87f357d0b440c5ff03aa6a0eefd1129a3b28d23
Balcihatice <aciceaybike@gmail.com>
2023-02-21 12:10:00 +0300

    Api-Test

--- src/test/java/com/apitest/day10/PostRequest.java
+++ src/test/java/com/apitest/day10/PostRequest.java
@@ -2,7 +2,9 @@ package com.apitest.day10;
 
 import com.apitest.testBase.DummyTestBase;
 import com.apitest.testData.DummyTestData;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -40,14 +42,20 @@ public class PostRequest extends DummyTestBase {
         HashMap<String, Object> expectedDataMap = obje.setUpExpectedData();//rsponse dan donen
         Response response = given().
                 accept("aapplication/json").
-                spec(spec03).body(requestBodyMap).
+                spec(spec03).auth().basic("admin", "password123").
                 when().post("/{parametre1}");
         response.prettyPrint();
 
+        //De Serialization
+        HashMap<String, Object> actualDataMap = response.as(HashMap.class);
+        Assert.assertEquals(expectedDataMap.get("statusCode"), response.getStatusCode());
+        Assert.assertEquals(expectedDataMap.get("status"), actualDataMap.get("status"));
+        Assert.assertEquals(expectedDataMap.get("message"), actualDataMap.get("message"));
 
+        //JsonPath
+        JsonPath jsonPath = response.jsonPath();
+        Assert.assertEquals(expectedDataMap.get("status"), jsonPath.getString("status"));
+        Assert.assertEquals(expectedDataMap.get("message"), jsonPath.getString("message"));
 
-
-
-        //
     }
 }
